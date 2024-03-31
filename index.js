bd6049f4188916f422c1c538d404f5e87d78386a
cngmutlu <113509940+cngmutlu@users.noreply.github.com>
2024-03-31 16:19:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthsOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+app.get("/api/:date", function(req, res) {
+  const date = new Date(req.params.date);
+  const unixData = date.getTime();
+  const utcYear = date.getUTCFullYear();
+  const utcMonth = date.getUTCMonth(); 
+  const utcDay = date.getUTCDate();
+  const utcDayNum = date.getUTCDay();
+  const utcHour = date.getUTCHours();
+  const utcMinute = date.getUTCMinutes();
+  const utcSecond = date.getUTCSeconds();
+  const utcData = daysOfWeek[utcDayNum] + ", " + utcDay + " " + monthsOfYear[utcMonth] + " " +
+   utcYear +  " " + utcHour + ":" + utcMinute + ":" + utcSecond + " GMT"; 
+  res.json({unix: unixData, utc: utcData});
+});
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
